cac9afc55c4d7f5c8dc2a29b863160b037327345
jonaldronc <jonaldron.custodia@kgb.com>
2018-10-25 19:36:31 +0800

    added put and get with parameters

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -2,11 +2,13 @@ Given(/^I want to get the users$/) do
   @request = 'get'
 end
 
+=begin
 When(/^I send an api GET request$/) do
   if @request == 'get'
     send_get(TestConfig['host'], '/api/users')
   end
 end
+=end
 
 Then(/^the response is a success$/) do
   p @response.code
@@ -50,6 +52,10 @@ When(/^I send an api request$/) do
       send_get(TestConfig['host'], '/api/users')
     when 'post'
       send_post(TestConfig['host'], '/api/users',@json)
+    when 'put'
+      send_put(TestConfig['host'], '/api/users/1',@json)
+    when 'get_with_parameters'
+      send_get_with_parameters(TestConfig['host'], '/api/users', @parameters)
     else
       raise('Request method not available')
   end
@@ -59,7 +65,48 @@ Then(/^the user is added$/) do
   p @response.code
   p @response.message
   #expect(@user.address[0].city == JSON.parse(@response.body)['address'][0]['city'])
+  expect(JSON.parse(@response.body)['first_name']).to eq(@user.first_name)
+  expect(JSON.parse(@response.body)['last_name']).to eq(@user.last_name)
   expect(JSON.parse(@response.body)['address'][0]['city']).to eq(@user.address[0].city)
+  expect(JSON.parse(@response.body)['createdAt'].to_s[0..9]).to eq(Time.now.to_s[0..9])
   id = JSON.parse(@response.body)['id']
   p "Your User Id is: #{id}"
+end
+
+Given(/^I want to update a user$/) do
+  @request = 'put'
+  @json = update_user
+
+end
+
+And(/^the user is updated$/) do
+  response = JSON.parse(@response.body)
+  p response
+
+  # expect(JSON.parse(@response.body)['first_name']).to eq(@user.first_name)
+  # expect(JSON.parse(@response.body)['last_name']).to eq(@user.last_name)
+  # expect(JSON.parse(@response.body)['address'][0]['house']).to eq(@user.address[0].house)
+  # expect(JSON.parse(@response.body)['updatedAt'].to_s[0..9]).to eq(Time.now.to_s[0..9])
+
+  expect(response['first_name']).to eq(@user.first_name)
+  expect(response['last_name']).to eq(@user.last_name)
+  expect(response['address'][0]['house']).to eq(@user.address[0].house)
+  expect(response['updatedAt'].to_s[0..9]).to eq(Time.now.to_s[0..9])
+end
+
+
+Given(/^I want to get the users with parameters$/) do
+  @request = 'get_with_parameters'
+end
+
+And(/^I want to get "([^"]*)" pages with "([^"]*)" users per page$/) do |page, number_of_users|
+  @parameters = "page=#{page}&per_page=#{number_of_users}"
+end
+
+And(/^the response displays "([^"]*)" pages with "([^"]*)" users per page$/) do |page, number_of_users|
+  response = JSON.parse(@response.body)
+  p response['page']
+  p response['per_page']
+  expect(response['page']).to eq(page.to_i)
+  expect(response['per_page']).to eq(number_of_users.to_i)
 end
